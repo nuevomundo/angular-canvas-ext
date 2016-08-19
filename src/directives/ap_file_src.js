290ec47dd939df2e507735d568be3417ce6b6275
Eric Garcia <eric.garcia@gmail.com>
2016-08-19 10:40:46 -0700

    minify

--- src/directives/ap_file_src.js
+++ src/directives/ap_file_src.js
@@ -9,22 +9,21 @@ canvasExtModule.directive('apFileSrc', function(apImageHelper) {
       quality: "=?"
     },
     link: function($scope, element, attrs) {
-      
+
       var updateImageSrc = function(src) {
-        console.log('new src ' + src);
         $scope.src = src;
         $scope.$apply();
         if ($scope.onImageReady) {
-          $scope.onImageReady();  
+          $scope.onImageReady();
         }
       };
 
       element.bind('change', function(e) {
         console.log('file changed');
         if ($scope.onImageSelected) {
-          $scope.onImageSelected();  
+          $scope.onImageSelected();
         }
-        
+
         var file = e.target.files.length ? e.target.files[0] : null;
         if (file) {
           apImageHelper.fileToImageDataURI(file, $scope.mimeType, $scope.quality, updateImageSrc);
